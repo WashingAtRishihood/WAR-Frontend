7c16916b78a4ae087a3c496ad5000244c63d684b
sankalpmtellur <sankalpmtellur282006@gmail.com>
2025-08-01 17:04:31 +0530

    Completed whole student workflow

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import HomePage from './pages/HomePage';
 import StudentLogin from './pages/student/StudentLogin';
 import StudentSignup from './pages/student/StudentSignup';
 import StudentDashboard from './pages/student/Dashboard';
+import StudentProfile from './pages/student/Profile';
+import Completed from './pages/student/Completed';
+import Incompleted from './pages/student/Incompleted';
+import AboutUs from './pages/AboutUs';
 
 function App() {
     return (
@@ -14,6 +18,11 @@ function App() {
                 <Route path="/student/login" element={<StudentLogin />} />
                 <Route path="/student/signup" element={<StudentSignup />} />
                 <Route path="/student/dashboard" element={<StudentDashboard />} />
+                <Route path="/student/profile" element={<StudentProfile />} />
+                <Route path="/student/completed" element={<Completed />} />
+                <Route path="/student/incompleted" element={<Incompleted />} />
+                <Route path="/student/dashboard" element={<StudentDashboard />} />
+                <Route path="/about-us" element={<AboutUs />} />
             </Routes>
         </Router>
     );
